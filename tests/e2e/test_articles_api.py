bc8c0f024714930d2a50f6537461f0f4649e8779
Tserewara <alvaro.alf2@gmail.com>
2021-02-26 16:29:50 -0300

    Add decorator to authenticate views and refactor e2e tests to use it

---
+++ tests/e2e/test_articles_api.py
@@ -0,0 +1,50 @@
+import pytest
+import requests
+
+from src.articles import config
+from tests.e2e.fake_authentication import authenticate
+
+
+@pytest.mark.usefixtures('restart_api')
+def test_happy_path_returns_201_and_article_title(postgres_session):
+    url = config.get_api_url()
+
+    fake_session = requests.Session()
+
+    authenticate(fake_session)
+
+    article = {
+        "title": "An article",
+        "description": "A remarkable description",
+        "content": "Some very useful content",
+        "tags": [],
+        "category_id": 1
+    }
+
+    r = fake_session.post(f'{url}/articles', json=article)
+
+    assert r.status_code == 201
+    assert r.json()['message'] == 'An article'
+
+
+@pytest.mark.usefixtures('restart_api')
+def test_unhappy_path_returns_404_and_error_message(postgres_session):
+    url = config.get_api_url()
+
+    fake_session = requests.Session()
+
+    authenticate(fake_session)
+
+    article = {
+        "title": "An article",
+        "description": "A remarkable description",
+        "content": "Some very useful content",
+        "tags": [],
+        "category_id": 1
+    }
+
+    fake_session.post(f'{url}/articles', json=article)
+    r = fake_session.post(f'{url}/articles', json=article)
+
+    assert r.status_code == 404
+    assert r.json()['message'] == 'Can\'t create article. Title duplicate.'
