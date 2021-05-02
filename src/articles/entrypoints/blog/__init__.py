73cdd2fc019cee5e0ad3646802457afe45ee3456
Tserewara <alvaro.alf2@gmail.com>
2021-05-02 14:42:24 -0300

    Refactor static_folder to static

--- src/articles/entrypoints/blog/__init__.py
+++ src/articles/entrypoints/blog/__init__.py
@@ -3,7 +3,8 @@ from flask import Blueprint
 blog = Blueprint('blog',
                  __name__,
                  template_folder='templates',
-                 static_folder='static_folder'
+                 static_folder='static',
+                 static_url_path='/blog'
                  )
 
 from .routes import blog
